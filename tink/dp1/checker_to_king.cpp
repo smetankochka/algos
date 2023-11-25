cad23409207ce33d4d3f70a4f3ecdf2bbcd4bf6f
smetanka <smetanochka@petalmail.com>
2023-11-25 19:18:40 +0700

    add road max cost and checker to king

--- tink/dp1/checker_to_king.cpp
+++ tink/dp1/checker_to_king.cpp
@@ -2,5 +2,17 @@
 #include <vector>
 using namespace std;
 int main() {
-    
+    vector<vector<int>> board(9, vector<int> (10, 0));
+    int n, m, ans = 0;
+    cin >> m >> n;
+    board[n][m] = 1;
+    for (int i = n + 1; i < 9; i++) {
+        for (int j = 1; j < 9; j++) {
+            board[i][j] += board[i - 1][j - 1] + board[i - 1][j + 1];
+        }
+    }
+    for (int i = 1; i < 9; i++) {
+        ans += board[8][i];
+    }
+    cout << ans;
 }
--- tink/dp1/road_max_cost.cpp
+++ tink/dp1/road_max_cost.cpp
@@ -1,34 +1,54 @@
 #include <iostream>
 #include <vector>
-using ll = long long;
+#include <string>
+#include <algorithm>
 using namespace std;
+
 int main() {
     int n, m;
-    ll cur;
     cin >> n >> m;
-    vector<vector<ll>> dp(n + 1, vector<ll> (m + 1, 0));
-    vector<vector<char>> prevcommand(n + 1, vector<char> (m + 1));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> cur;
-            if (dp[i - 1][j] > dp[i][j - 1]) {
-                dp[i][j] = dp[i - 1][j] + cur;
-                prevcommand[i][j] = 'D';
+    vector<vector<int>> table(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> table[i][j];
+        }
+    }
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    vector<vector<char>> prev(n, vector<char>(m, '0'));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = table[i][j];
+            } else if (i == 0) {
+                dp[i][j] = dp[i][j - 1] + table[i][j];
+                prev[i][j] = 'R';
+            } else if (j == 0) {
+                dp[i][j] = dp[i - 1][j] + table[i][j];
+                prev[i][j] = 'D';
             } else {
-                dp[i][j] = dp[i][j - 1] + cur;
-                prevcommand[i][j] = 'R';
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    dp[i][j] = dp[i - 1][j] + table[i][j];
+                    prev[i][j] = 'D';
+                } else {
+                    dp[i][j] = dp[i][j - 1] + table[i][j];
+                    prev[i][j] = 'R';
+                }
             }
         }
     }
-    cout << dp[n][m] << "\n";
-    size_t curi = n, curj = m;
-    for (int i = 0; i < n + m - 2; i++) {
-        cout << prevcommand[curi][curj] << " ";
-        if (prevcommand[curi][curj] == 'D') {
+    cout << dp[n - 1][m - 1] << "\n";
+    int curi = n - 1, curj = m - 1;
+    string path = "";
+    while (curi > 0 || curj > 0) {
+        path = prev[curi][curj] + path;
+        if (prev[curi][curj] == 'D') {
             curi--;
         } else {
             curj--;
         }
+    }
+    for (int i = 0; i < path.size(); i++) {
+        cout << path[i] << " ";
     }
     return 0;
 }
