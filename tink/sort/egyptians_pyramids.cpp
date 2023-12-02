5c1b3d57fcd80f2d17428504dc6ee7c85c074aab
smetanka <smetanochka@petalmail.com>
2023-12-02 19:30:33 +0700

    completed egyptians pyramids

--- tink/sort/egyptians_pyramids.cpp
+++ tink/sort/egyptians_pyramids.cpp
@@ -1,1 +1,29 @@
-init
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, min_y, trash;
+    cin >> n;
+    vector<pair<int, int>> polys(n);
+    for (int i = 0; i < n; i++) {
+        cin >> m;
+        cin >> trash >> polys[i].first;
+        m--;
+        while (m--) {
+            cin >> trash >> min_y;
+            if (min_y < polys[i].first) {polys[i].first = min_y;}
+        }
+        polys[i].second = i;
+    }
+    sort(polys.begin(), polys.end());
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++) {
+        ans[polys[i].second] = i;
+    }
+    for (int i = 0; i < n; i++) {cout << ans[i] << " ";}
+}
