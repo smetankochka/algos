cc344e12addb69a59e5a7ddda2b550dff281d2bd
smetanka <smetanochka@petalmail.com>
2023-12-08 22:56:28 +0700

    completed linear algorithms

--- tink/linal/prizes.cpp
+++ tink/linal/prizes.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main() {
     int n, k;
-    cin >> n >> k;
-    vector<int> values(n);
-    for (int i = 0; i < n; i++) {
-        cin >> values[i];
-    }
-    vector<int> prefixSum(n + 1, 0);
+    std::cin >> n >> k;
+    std::vector<long long> a(n + 1);
+    std::vector<long long> s(n + 1, 0);
+    std::vector<long long> pref(n + 1, 0);
+    std::vector<long long> suff(n + 2, 0);
+
     for (int i = 1; i <= n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + values[i - 1];
+        std::cin >> a[i];
+        s[i] = s[i - 1] + a[i];
     }
-    vector<int> pref(n + 1, 0);
-    vector<int> suff(n + 1, 0);
+
+    for (int i = k; i <= n; i++) {
+        pref[i] = std::max(pref[i - 1], s[i] - s[i - k]);
+    }
+
+    for (int i = n - k + 1; i >= 1; i--) {
+        suff[i] = std::max(suff[i + 1], s[i + k - 1] - s[i - 1]);
+    }
+
+    long long best = 2e18;
     for (int i = 1; i <= n - k + 1; i++) {
-        pref[i] = max(pref[i - 1], prefixSum[i + k - 1] - prefixSum[i - 1]);
+        best = std::min(best, std::max(pref[i - 1], suff[i + k]));
     }
-    for (int i = n - k; i >= 1; i--) {
-        suff[i] = max(suff[i + 1], prefixSum[i + k] - prefixSum[i]);
-    }
-    int result = 1e9;
-    for (int i = 1; i <= n - k + 1; i++) {
-        result = min(result, max(pref[i - 1], suff[i + k]));
-    }
-    cout << result << endl;
+
+    std::cout << best << std::endl;
+
     return 0;
 }
