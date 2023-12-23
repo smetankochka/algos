51587730da479805dfe4a68019d3916c3bb0fd8e
smetanka <smetanochka@petalmail.com>
2023-12-23 16:11:19 +0700

    start exam

---
+++ tink/exam_1/task1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by smetanka on 23.12.23.
+//
+#include "iostream"
+#include "algorithm"
+#include "vector"
+#include "deque"
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
