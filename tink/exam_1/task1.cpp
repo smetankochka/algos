f715575a6fb5456687b12d3b2d6a20b06a3834f5
smetanka <smetanochka@petalmail.com>
2023-12-23 16:19:22 +0700

    add first

--- tink/exam_1/task1.cpp
+++ tink/exam_1/task1.cpp
@@ -1,6 +1,3 @@
-//
-// Created by smetanka on 23.12.23.
-//
 #include "iostream"
 #include "algorithm"
 #include "vector"
@@ -13,6 +10,21 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<string> picture(n);
+    for (int i = 0; i < n; i++) {
+        cin >> picture[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int p = 0; p < k; p++) {
+            for (int j = 0; j < m; j++) {
+                for (int q = 0; q < k; q++) {
+                    cout << picture[i][j];
+                }
+            }
+            cout << "\n";
+        }
+    }
     return 0;
 }
