4a3f856fd30f4abe621f789ddbad9697c8f197bf
smetanka <smetanochka@petalmail.com>
2024-01-22 23:43:17 +0700

    upd

--- tink/scanline/seg_on_line.cpp
+++ tink/scanline/seg_on_line.cpp
@@ -1,59 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <stack>
+#include <set>
+using namespace std;
 
-struct Event {
+struct point {
     int x;
-    int type; // 0 - начало отрезка, 1 - конец отрезка
-    int index;
-
-    bool operator<(const Event& other) const {
-        if (x == other.x) {
-            return type < other.type;
-        }
-        return x < other.x;
-    }
+    int y;
+    bool start;
+    int number;
 };
 
+bool comp(point a, point b) {
+    if (a.x == b.x) {
+        if (a.start && b.start) {
+            return a.y > b.y;
+        }
+        return a.start > b.start;
+    } 
+    return a.x < b.x;
+}
+
 int main() {
-    int N;
-    std::cin >> N;
-
-    std::vector<std::pair<int, int>> segments(N);
-    for (int i = 0; i < N; ++i) {
-        std::cin >> segments[i].first >> segments[i].second;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<point> mas(2 * n);
+    vector<int> ans(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> mas[2 * i].x >> mas[2 * i + 1].x;
+        mas[2 * i].y = mas[2 * i + 1].x;
+        mas[2 * i + 1].y = mas[2 * i].x;
+        mas[2 * i].start = true;
+        mas[2 * i + 1].start = false;
+        mas[2 * i].number = i + 1;
+        mas[2 * i + 1].number = i + 1;
     }
-
-    std::vector<Event> events; // список событий
-    for (int i = 0; i < N; ++i) {
-        events.push_back({segments[i].first, 0, i}); // начало отрезка
-        events.push_back({segments[i].second, 1, i}); // конец отрезка
-    }
-
-    std::sort(events.begin(), events.end()); // сортировка событий по X
-
-    std::vector<int> result(N, -1); // результат для каждого отрезка, -1 означает отсутствие результата
-    std::stack<int> stack; // стек для хранения непосредственно содержащихся отрезков
-
-    for (const auto& event : events) {
-        if (event.type == 0) { // начало отрезка
-            stack.push(event.index);
-        } else { // конец отрезка
-            if (!stack.empty()) {
-                int top = stack.top();
-                stack.pop();
-                if (stack.empty() || segments[top].second >= segments[stack.top()].second) {
-                    result[event.index] = top;
+    sort(mas.begin(), mas.end(), comp);
+    set<pair<int, pair<int, int>>> opened;
+    for (int i = 0; i < 2 * n; i++) {
+        if (mas[i].start) {
+            if (!opened.empty() && (*opened.rbegin()).first >= mas[i].y) {
+                if (((*opened.rbegin()).first == mas[i].y) && (*opened.rbegin()).second.first == mas[i].x) {
+                    ans[(*opened.rbegin()).second.second - 1] = mas[i].number;
                 }
+                ans[mas[i].number - 1] = (*opened.rbegin()).second.second;
             }
+            opened.insert(make_pair(mas[i].y, make_pair(mas[i].x, mas[i].number)));
+        } else {
+            opened.erase(make_pair(mas[i].y, make_pair(mas[i].x, mas[i].number)));
         }
     }
-
-    for (const auto& res : result) {
-        std::cout << res + 1 << " "; // вывод результатов (+1 для приведения индексов к единой нумерации)
+    for (int a : ans) {
+        cout << a << " ";
     }
-    std::cout << std::endl;
-
     return 0;
 }
