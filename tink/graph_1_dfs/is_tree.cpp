0412d65a6f49350d974e037d2447b88833b2495e
smetanka <smetanochka@petalmail.com>
2024-01-27 17:48:10 +0700

    add is tree

---
+++ tink/graph_1_dfs/is_tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(int v, int parent, vector<bool> &visited, vector<vector<int>> &graph) {
+    visited[v] = true;
+    for (int i = 0; i < graph[v].size(); i++) {
+        int to = graph[v][i];
+        if (!visited[to]) {
+            dfs(to, v, visited, graph);
+        } else if (to != parent) {
+            cout << "NO";
+            exit(0);
+        }
+    }
+}
+
+int main() {
+    int n, m, u, v;
+    cin >> n >> m;
+    vector<bool> visited(n, false);
+    vector<vector<int>> graph(n);
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v;
+        graph[u - 1].push_back(v - 1);
+        graph[v - 1].push_back(u - 1);
+    }
+    int components = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i, -1, visited, graph);
+            components++;
+        }
+    }
+
+    if (m == n - 1 && components == 1) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
