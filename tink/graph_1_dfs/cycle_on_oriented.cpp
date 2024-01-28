60b1a70cddd0d225ec902d43c1d4dd15085edee5
smetanka <smetanochka@petalmail.com>
2024-01-28 15:58:17 +0700

    add cycle on oriented graph

--- tink/graph_1_dfs/cycle_on_oriented.cpp
+++ tink/graph_1_dfs/cycle_on_oriented.cpp
@@ -1,27 +1,40 @@
+
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-void dfs(vector<vector<int>> &graph, vector<bool> &visited, int v) {
-    if (visited[v]) {
-        
+bool dfs(vector<vector<int>>& graph, vector<int>& state, int v) {
+    state[v] = 1;
+    for (int u : graph[v]) {
+        if (state[u] == 0) {
+            if (dfs(graph, state, u)) {
+                return true;
+            }
+        } else if (state[u] == 1) {
+            return true;
+        }
     }
+    state[v] = 2;
+    return false;
 }
 
 int main() {
-    int n, m, v, u;
+    int n, m;
     cin >> n >> m;
     vector<vector<int>> graph(n);
     for (int i = 0; i < m; i++) {
+        int u, v;
         cin >> v >> u;
         graph[v - 1].push_back(u - 1);
     }
-    vector<int> visited(n, 0);
+    vector<int> state(n, 0);
     for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            dfs(graph, visited, i);
+        if (state[i] == 0 && dfs(graph, state, i)) {
+            cout << 1;
+            return 0;
         }
     }
+    cout << 0;
     return 0;
 }
