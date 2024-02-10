0567dc44e931614ec05f8146cd6326d3feabc224
smetanka <smetanochka@petalmail.com>
2024-02-10 20:38:02 +0700

    complete pifagors train

--- tink/graph_2_bfs/pifagor-dijkstra.cpp
+++ tink/graph_2_bfs/pifagor-dijkstra.cpp
@@ -3,12 +3,13 @@
 #include <deque>
 #include <cmath>
 #include <set>
+#include <climits>
+const int INF = INT_MAX;
 using namespace std;
 
 int main() {
-    int n, s, t, v;
+    int n, s, t, v, w;
     cin >> n;
-    vector<vector<pair<int, int>>> g(n);
     vector<pair<int, int>> input(n);
     for (int i = 0; i < n; i++) {
         cin >> input[i].first >> input[i].second;
@@ -16,26 +17,30 @@ int main() {
     cin >> s >> t;
     s--;
     t--;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i != j) {
-                g[i].push_back({pow(input[i].first - input[j].first, 2) + pow(input[i].second - input[j].second, 2), j});
-            }
-        }
-    }
     vector<int> dist(n, 1e9);
-    set<pair<int, int>> q;
+    vector<bool> is_visited(n, false);
     dist[s] = 0;
-    q.insert({0, s});
-    while (!q.empty()) {
-        v = q.begin()->second;
-        q.erase(q.begin());
-        for (auto [w, u] : g[v]) {
-            if (dist[u] > dist[v] + w) {
-                q.erase({dist[u], u});
-                dist[u] = dist[v] + w;
-                q.insert({dist[u], u});
+    for (int iter_n = 0; iter_n < n; ++iter_n) {
+        // ============== Выбираем ближайшую непосещённую ==============
+        int closest_not_visited = -1;
+        int closest_dist = INF;
+        for (int v = 0; v < n; ++v) {
+            if (!is_visited[v] && dist[v] < closest_dist) {
+                closest_dist = dist[v];
+                closest_not_visited = v;
+            }  
+        }
+        
+        is_visited[closest_not_visited] = true;
+
+        // ============== Релаксируем рёбра, идущие из неё ==============
+
+        for (int to = 0; to < n; ++to) {
+            if (to == closest_not_visited) {
+                continue;
             }
+            w = pow(input[to].first - input[closest_not_visited].first, 2) + pow(input[to].second - input[closest_not_visited].second, 2);
+            dist[to] = min(dist[to], dist[closest_not_visited] + w);
         }
     }
     cout << dist[t];
