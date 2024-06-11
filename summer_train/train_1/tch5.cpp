afb72807f0f9df644becb1599096bb95361830bc
smetanka <mmura997@yandex.by>
2024-06-11 12:56:37 +1000

    add some theory of num

---
+++ summer_train/train_1/tch5.cpp
@@ -0,0 +1,24 @@
+// powered by smetanka on 09.06.2024
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n, m = 1e6 + 3, f = 1;
+
+    cin >> n;
+
+    for (ll i = 2; i <= n; i++) {
+        f = (f * i) % m;
+    }
+
+    cout << f;
+
+    return 0;
+}
