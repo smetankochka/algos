bdf8b8a2890bf0b92a079956c2f810630072b081
$m3ť@nk@ <148964894+smetankochka@users.noreply.github.com>
2024-06-16 21:25:55 +1000

    Update and rename N2.py to N2.cpp

---
+++ summer_train/fefu/N2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    string text;
+    getline(input, text);  // Читаем первую строку (текст)
+
+    int N;
+    input >> N;
+
+    vector<string> words(N);   // Вектор для хранения слов
+    for (int i = 0; i < N; ++i) {
+        input >> words[i]; // читаем слова в вектор
+    }
+
+    vector<int> result(N); // Вектор для хранения результата
+    for (int i = 0; i < N; ++i) {
+        if (text.find(words[i]) != string::npos) { // просто есть ли слово в тексте с помощью встроенной функции 
+            result[i] = 1;
+        } else {
+            result[i] = 0;
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        output << result[i] << " "; // записываем результат
+    }
+
+    return 0;
+}
