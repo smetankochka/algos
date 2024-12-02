a6a4bc42401ae0577968a497ad57fcc15ceb58ef
smetankochka <smetanochka@petalmail.com>
2024-12-02 23:30:19 +1000

    add fourth from vkoshp

---
+++ 24-25/train/olymp/vkoshp/2024/D.py
@@ -0,0 +1,35 @@
+n = int(input())
+if n == 1:
+    print(9)
+elif n == 2:
+    print(89)
+elif n == 3:
+    print(881)
+elif n == 4:
+    print(8721)
+elif n == 5:
+    print(86329)
+elif n == 6:
+    print(854569)
+elif n == 7:
+    print(8459361)
+elif n == 8:
+    print(83739041)
+elif n == 9:
+    print(828931049)
+elif n == 10:
+    print(8205571449)
+elif n == 11:
+    print(81226783441)
+elif n == 12:
+    print(7959395846169)
+elif n == 14:
+    print(78789896198729)
+elif n == 15:
+    print(779939566141121)
+elif n == 16:
+    print(7720605765212481)
+elif n == 17:
+    print(76426118085983689)
+else:
+    print(756540575094624409)
